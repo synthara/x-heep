143e183185966e253041a46cceb676b484ad3f98
Ludovico Carraria <ludovico.carraria@synthara.ai>
2024-10-07 16:45:22 +0200

    Changed CXR instance to BRAM instance

--- hw/synthara/memory_subsystem.sv
+++ hw/synthara/memory_subsystem.sv
@@ -29,14 +29,6 @@ module memory_subsystem
   // Clock-gating
   logic [NUM_BANKS-1:0] clk_cg;
 
-  logic [13-1:0] ram_req_addr_0;
-  logic [13-1:0] ram_req_addr_1;
-
-
-  assign ram_req_addr_0 = ram_req_i[0].addr[15-1:2];
-
-  assign ram_req_addr_1 = ram_req_i[1].addr[15-1:2];
-
   for (genvar i = 0; i < NUM_BANKS; i++) begin : gen_sram
 
     tc_clk_gating clk_gating_cell_i (
@@ -61,15 +53,21 @@ module memory_subsystem
   // TODO: temp parameters to allow compilation
   parameter TB_AXI4_WORD_NUM_BITS = 32;
   parameter TB_AXI4_ADDRESS_NUM_BITS = 32;
+  
+  localparam BRAM_ADDR_NUM_BITS = 10;
+
+  localparam XPM_MEM_SIZE = 2**BRAM_ADDR_NUM_BITS * TB_AXI4_WORD_NUM_BITS;
+
   // Create the rule for the address map: start address is the staring address as per linker script
   // The addr_length defines the addressable storage space in each slave ComputeRAM
   parameter longint unsigned ADDR_MAP_0_START = 32'h80000000;
   // 12 bits used to address the 32bits word in the storage part of the ComputeRAM
   // 14 bits used to address the 1byte word in the storage part of the ComputeRAM
-  parameter longint unsigned STORAGE_SPACE_BYTE_SIZE = (4096 - 0) * 4;
+  parameter longint unsigned STORAGE_SPACE_BYTE_SIZE = (1024 - 0) * 4;
 
   logic[NUM_BANKS-1:0][TB_AXI4_WORD_NUM_BITS-1:0] axi_mem_wdata_o;
-  logic[NUM_BANKS-1:0][14-1:0]                    axi_mem_addr_14b;
+  // logic[NUM_BANKS-1:0][14-1:0]                    axi_mem_addr_14b;
+  logic[NUM_BANKS-1:0][BRAM_ADDR_NUM_BITS-1:0]                    axi_mem_addr_10b;
   logic[NUM_BANKS-1:0][TB_AXI4_WORD_NUM_BITS-1:0] axi_mem_be_32b, r_axi_mem_be_32b;
   logic[NUM_BANKS-1:0]                            axi_mem_we_o;
   logic[NUM_BANKS-1:0]                            axi_mem_req_o;
@@ -82,19 +80,78 @@ module memory_subsystem
   logic[NUM_BANKS-1:0] axi_mem_gnt_i;
   logic[NUM_BANKS-1:0] axi_mem_rvalid_i;
 
+  logic pos_rst;
 
-  cxr_ssm ram0_i(
-      // Connect ports using the specified format
-      .CLK(clk_cg[0]),
-      .RSTN(rst_ni),
-      .D(axi_mem_wdata_o[0]),  // write data
-      .A(axi_mem_addr_14b[0]),  // address common for read and write
-      .BW(axi_mem_be_32b[0]),  // byte write enable
-      .RDWEN(!axi_mem_we_o[0]),  // read-write enable read=1 write=0
-      .CEN(!axi_mem_req_o[0]),  // chip enable active low
-      .Q(axi_mem_rdata_i[0]),  // read data
-      .IRQ(IRQ[0])
-  );
+  always_comb pos_rst = !rst_ni;
+  always_comb IRQ = '0;
+
+
+  // cxr_ssm ram0_i(
+  //     // Connect ports using the specified format
+  //     .CLK(clk_cg[0]),
+  //     .RSTN(rst_ni),
+  //     .D(axi_mem_wdata_o[0]),  // write data
+  //     .A(axi_mem_addr_14b[0]),  // address common for read and write
+  //     .BW(axi_mem_be_32b[0]),  // byte write enable
+  //     .RDWEN(!axi_mem_we_o[0]),  // read-write enable read=1 write=0
+  //     .CEN(!axi_mem_req_o[0]),  // chip enable active low
+  //     .Q(axi_mem_rdata_i[0]),  // read data
+  //     .IRQ(IRQ[0])
+  // );
+
+      // xpm_memory_spram: Single Port RAM
+    // Xilinx Parameterized Macro, version 2021.2
+    xpm_memory_spram#(
+        .ADDR_WIDTH_A       (BRAM_ADDR_NUM_BITS),  // DECIMAL
+        .AUTO_SLEEP_TIME    (0),  // DECIMAL
+        .BYTE_WRITE_WIDTH_A (8),  // DECIMAL
+        .CASCADE_HEIGHT     (0),  // DECIMAL
+        .ECC_MODE           ("no_ecc"),  // String
+        .MEMORY_INIT_FILE   ("none"),  // String
+        .MEMORY_INIT_PARAM  ("0"),  // String
+        .MEMORY_OPTIMIZATION("true"),  // String
+        .MEMORY_PRIMITIVE   ("block"),  // String
+        .MEMORY_SIZE        (XPM_MEM_SIZE),  // Specify the total memory array size, in bits. For example, enter 65536 for a 2kx32 RAM.
+        .MESSAGE_CONTROL    (0),  // DECIMAL
+        .READ_DATA_WIDTH_A  (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .READ_LATENCY_A     (1),  // DECIMAL
+        .READ_RESET_VALUE_A ("0"),  // String
+        .RST_MODE_A         ("ASYNC"),  // String
+        .SIM_ASSERT_CHK     (0),  // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+        .USE_MEM_INIT       (0),  // DECIMAL
+        .USE_MEM_INIT_MMI   (0),  // DECIMAL
+        .WAKEUP_TIME        ("disable_sleep"),  // String
+        .WRITE_DATA_WIDTH_A (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .WRITE_MODE_A       ("read_first"),  // String
+        .WRITE_PROTECT      (1)  // DECIMAL
+    ) xpm_memory_spram_inst_0 (
+        .clka (clk_cg[0]),  // 1-bit input: Clock signal for port A.
+        .rsta (pos_rst),  // 1-bit input: Reset signal for the final port A output register stage.
+        // Synchronously resets output port douta to the value specified by
+        // parameter READ_RESET_VALUE_A.
+        .ena  (axi_mem_req_o[0]),  // 1-bit input: Memory enable signal for port A. Must be high on clock
+        // cycles when read or write operations are initiated. Pipelined internally.
+        .addra(axi_mem_addr_10b[0]),  // ADDR_WIDTH_A-bit input: Address for port A write and read operations.
+        .dina (axi_mem_wdata_o[0]),  // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+        .wea  (axi_mem_be_32b[0]),  // WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+        // for port A input data port dina. 1 bit wide when word-wide writes are
+        // used. In byte-wide write configurations, each bit controls the
+        // writing one byte of dina to address addra. For example, to
+        // synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+        // is 32, wea would be 4'b0010.
+        .douta(axi_mem_rdata_i[0]),  // READ_DATA_WIDTH_A-bit output: Data output for port A read operations.
+
+        .dbiterra      (),  // 1-bit output: Status signal to indicate double bit error occurrence
+        // on the data output of port A.
+        .sbiterra      (),  // 1-bit output: Status signal to indicate single bit error occurrence
+        // on the data output of port A.
+        .injectdbiterra(1'b0),  // 1-bit input: Controls double bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .injectsbiterra(1'b0),  // 1-bit input: Controls single bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .regcea        (1'b1),  // 1-bit input: Clock Enable for the last register stage on the output data path.
+        .sleep         (1'b0)  // 1-bit input: sleep signal to enable the dynamic power saving feature.
+    );
 
   always_comb begin
     // Extend the mem_strb_o from 4 bits to 32 bits, each bit must be extended 8 times
@@ -117,7 +174,8 @@ module memory_subsystem
         specific_offset = STORAGE_SPACE_BYTE_SIZE 
         shift = 2 to pass from byte addr to word addr
     */
-    axi_mem_addr_14b[0] =  ( axi_mem_addr_o[0] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 0  )  >> 2;
+    // axi_mem_addr_14b[0] =  ( axi_mem_addr_o[0] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 0  )  >> 2;
+    axi_mem_addr_10b[0] =  ( axi_mem_addr_o[0] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 0  )  >> 2;
     /* axi_mem_we_o = !RDWEN    AND   axi_mem_req_o = !CE  means a read in this clock cycle */
     axi_mem_rvalid_n[0] =  /* axi_mem_we_o[0] && */ axi_mem_req_o[0];
     /* memory always responds after 1 clock cycle for write and read so, if we just delay the reuqest signal to have the gnt */
@@ -137,18 +195,72 @@ module memory_subsystem
   end
 
 
-  cxr_ssm ram1_i(
-      // Connect ports using the specified format
-      .CLK(clk_cg[1]),
-      .RSTN(rst_ni),
-      .D(axi_mem_wdata_o[1]),  // write data
-      .A(axi_mem_addr_14b[1]),  // address common for read and write
-      .BW(axi_mem_be_32b[1]),  // byte write enable
-      .RDWEN(!axi_mem_we_o[1]),  // read-write enable read=1 write=0
-      .CEN(!axi_mem_req_o[1]),  // chip enable active low
-      .Q(axi_mem_rdata_i[1]),  // read data
-      .IRQ(IRQ[1])
-  );
+  // cxr_ssm ram1_i(
+  //     // Connect ports using the specified format
+  //     .CLK(clk_cg[1]),
+  //     .RSTN(rst_ni),
+  //     .D(axi_mem_wdata_o[1]),  // write data
+  //     .A(axi_mem_addr_14b[1]),  // address common for read and write
+  //     .BW(axi_mem_be_32b[1]),  // byte write enable
+  //     .RDWEN(!axi_mem_we_o[1]),  // read-write enable read=1 write=0
+  //     .CEN(!axi_mem_req_o[1]),  // chip enable active low
+  //     .Q(axi_mem_rdata_i[1]),  // read data
+  //     .IRQ(IRQ[1])
+  // );
+
+      // xpm_memory_spram: Single Port RAM
+    // Xilinx Parameterized Macro, version 2021.2
+    xpm_memory_spram#(
+        .ADDR_WIDTH_A       (BRAM_ADDR_NUM_BITS),  // DECIMAL
+        .AUTO_SLEEP_TIME    (0),  // DECIMAL
+        .BYTE_WRITE_WIDTH_A (8),  // DECIMAL
+        .CASCADE_HEIGHT     (0),  // DECIMAL
+        .ECC_MODE           ("no_ecc"),  // String
+        .MEMORY_INIT_FILE   ("none"),  // String
+        .MEMORY_INIT_PARAM  ("0"),  // String
+        .MEMORY_OPTIMIZATION("true"),  // String
+        .MEMORY_PRIMITIVE   ("block"),  // String
+        .MEMORY_SIZE        (XPM_MEM_SIZE),  // Specify the total memory array size, in bits. For example, enter 65536 for a 2kx32 RAM.
+        .MESSAGE_CONTROL    (0),  // DECIMAL
+        .READ_DATA_WIDTH_A  (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .READ_LATENCY_A     (1),  // DECIMAL
+        .READ_RESET_VALUE_A ("0"),  // String
+        .RST_MODE_A         ("ASYNC"),  // String
+        .SIM_ASSERT_CHK     (0),  // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+        .USE_MEM_INIT       (0),  // DECIMAL
+        .USE_MEM_INIT_MMI   (0),  // DECIMAL
+        .WAKEUP_TIME        ("disable_sleep"),  // String
+        .WRITE_DATA_WIDTH_A (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .WRITE_MODE_A       ("read_first"),  // String
+        .WRITE_PROTECT      (1)  // DECIMAL
+    ) xpm_memory_spram_inst_1 (
+        .clka (clk_cg[1]),  // 1-bit input: Clock signal for port A.
+        .rsta (pos_rst),  // 1-bit input: Reset signal for the final port A output register stage.
+        // Synchronously resets output port douta to the value specified by
+        // parameter READ_RESET_VALUE_A.
+        .ena  (axi_mem_req_o[1]),  // 1-bit input: Memory enable signal for port A. Must be high on clock
+        // cycles when read or write operations are initiated. Pipelined internally.
+        .addra(axi_mem_addr_10b[1]),  // ADDR_WIDTH_A-bit input: Address for port A write and read operations.
+        .dina (axi_mem_wdata_o[1]),  // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+        .wea  (axi_mem_be_32b[1]),  // WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+        // for port A input data port dina. 1 bit wide when word-wide writes are
+        // used. In byte-wide write configurations, each bit controls the
+        // writing one byte of dina to address addra. For example, to
+        // synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+        // is 32, wea would be 4'b0010.
+        .douta(axi_mem_rdata_i[1]),  // READ_DATA_WIDTH_A-bit output: Data output for port A read operations.
+
+        .dbiterra      (),  // 1-bit output: Status signal to indicate double bit error occurrence
+        // on the data output of port A.
+        .sbiterra      (),  // 1-bit output: Status signal to indicate single bit error occurrence
+        // on the data output of port A.
+        .injectdbiterra(1'b0),  // 1-bit input: Controls double bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .injectsbiterra(1'b0),  // 1-bit input: Controls single bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .regcea        (1'b1),  // 1-bit input: Clock Enable for the last register stage on the output data path.
+        .sleep         (1'b0)  // 1-bit input: sleep signal to enable the dynamic power saving feature.
+    );
 
   always_comb begin
     // Extend the mem_strb_o from 4 bits to 32 bits, each bit must be extended 8 times
@@ -171,7 +283,8 @@ module memory_subsystem
         specific_offset = STORAGE_SPACE_BYTE_SIZE 
         shift = 2 to pass from byte addr to word addr
     */
-    axi_mem_addr_14b[1] =  ( axi_mem_addr_o[1] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 1  )  >> 2;
+    // axi_mem_addr_14b[1] =  ( axi_mem_addr_o[1] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 1  )  >> 2;
+    axi_mem_addr_10b[1] =  ( axi_mem_addr_o[1] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * 1  )  >> 2;
     /* axi_mem_we_o = !RDWEN    AND   axi_mem_req_o = !CE  means a read in this clock cycle */
     axi_mem_rvalid_n[1] =  /* axi_mem_we_o[1] && */ axi_mem_req_o[1];
     /* memory always responds after 1 clock cycle for write and read so, if we just delay the reuqest signal to have the gnt */
--- hw/synthara/memory_subsystem.sv.tpl
+++ hw/synthara/memory_subsystem.sv.tpl
@@ -29,18 +29,6 @@ module memory_subsystem
   // Clock-gating
   logic [NUM_BANKS-1:0] clk_cg;
 
-% for i, bank in enumerate(xheep.iter_ram_banks()):
-  logic [${bank.size().bit_length()-1 -2}-1:0] ram_req_addr_${i};
-% endfor
-
-% for i, bank in enumerate(xheep.iter_ram_banks()):
-<%
-  p1 = bank.size().bit_length()-1 + bank.il_level()
-  p2 = 2 + bank.il_level()
-%>
-  assign ram_req_addr_${i} = ram_req_i[${i}].addr[${p1}-1:${p2}];
-% endfor
-
   for (genvar i = 0; i < NUM_BANKS; i++) begin : gen_sram
 
     tc_clk_gating clk_gating_cell_i (
@@ -65,15 +53,21 @@ module memory_subsystem
   // TODO: temp parameters to allow compilation
   parameter TB_AXI4_WORD_NUM_BITS = 32;
   parameter TB_AXI4_ADDRESS_NUM_BITS = 32;
+  
+  localparam BRAM_ADDR_NUM_BITS = 10;
+
+  localparam XPM_MEM_SIZE = 2**BRAM_ADDR_NUM_BITS * TB_AXI4_WORD_NUM_BITS;
+
   // Create the rule for the address map: start address is the staring address as per linker script
   // The addr_length defines the addressable storage space in each slave ComputeRAM
   parameter longint unsigned ADDR_MAP_0_START = 32'h80000000;
   // 12 bits used to address the 32bits word in the storage part of the ComputeRAM
   // 14 bits used to address the 1byte word in the storage part of the ComputeRAM
-  parameter longint unsigned STORAGE_SPACE_BYTE_SIZE = (4096 - 0) * 4;
+  parameter longint unsigned STORAGE_SPACE_BYTE_SIZE = (1024 - 0) * 4;
 
   logic[NUM_BANKS-1:0][TB_AXI4_WORD_NUM_BITS-1:0] axi_mem_wdata_o;
-  logic[NUM_BANKS-1:0][14-1:0]                    axi_mem_addr_14b;
+  // logic[NUM_BANKS-1:0][14-1:0]                    axi_mem_addr_14b;
+  logic[NUM_BANKS-1:0][BRAM_ADDR_NUM_BITS-1:0]                    axi_mem_addr_10b;
   logic[NUM_BANKS-1:0][TB_AXI4_WORD_NUM_BITS-1:0] axi_mem_be_32b, r_axi_mem_be_32b;
   logic[NUM_BANKS-1:0]                            axi_mem_we_o;
   logic[NUM_BANKS-1:0]                            axi_mem_req_o;
@@ -86,20 +80,79 @@ module memory_subsystem
   logic[NUM_BANKS-1:0] axi_mem_gnt_i;
   logic[NUM_BANKS-1:0] axi_mem_rvalid_i;
 
+  logic pos_rst;
+
+  always_comb pos_rst = !rst_ni;
+  always_comb IRQ = '0;
+
 %for i, bank in enumerate(xheep.iter_ram_banks()):
 
-  cxr_ssm ram${i}_i(
-      // Connect ports using the specified format
-      .CLK(clk_cg[${i}]),
-      .RSTN(rst_ni),
-      .D(axi_mem_wdata_o[${i}]),  // write data
-      .A(axi_mem_addr_14b[${i}]),  // address common for read and write
-      .BW(axi_mem_be_32b[${i}]),  // byte write enable
-      .RDWEN(!axi_mem_we_o[${i}]),  // read-write enable read=1 write=0
-      .CEN(!axi_mem_req_o[${i}]),  // chip enable active low
-      .Q(axi_mem_rdata_i[${i}]),  // read data
-      .IRQ(IRQ[${i}])
-  );
+  // cxr_ssm ram${i}_i(
+  //     // Connect ports using the specified format
+  //     .CLK(clk_cg[${i}]),
+  //     .RSTN(rst_ni),
+  //     .D(axi_mem_wdata_o[${i}]),  // write data
+  //     .A(axi_mem_addr_14b[${i}]),  // address common for read and write
+  //     .BW(axi_mem_be_32b[${i}]),  // byte write enable
+  //     .RDWEN(!axi_mem_we_o[${i}]),  // read-write enable read=1 write=0
+  //     .CEN(!axi_mem_req_o[${i}]),  // chip enable active low
+  //     .Q(axi_mem_rdata_i[${i}]),  // read data
+  //     .IRQ(IRQ[${i}])
+  // );
+
+      // xpm_memory_spram: Single Port RAM
+    // Xilinx Parameterized Macro, version 2021.2
+    xpm_memory_spram#(
+        .ADDR_WIDTH_A       (BRAM_ADDR_NUM_BITS),  // DECIMAL
+        .AUTO_SLEEP_TIME    (0),  // DECIMAL
+        .BYTE_WRITE_WIDTH_A (8),  // DECIMAL
+        .CASCADE_HEIGHT     (0),  // DECIMAL
+        .ECC_MODE           ("no_ecc"),  // String
+        .MEMORY_INIT_FILE   ("none"),  // String
+        .MEMORY_INIT_PARAM  ("0"),  // String
+        .MEMORY_OPTIMIZATION("true"),  // String
+        .MEMORY_PRIMITIVE   ("block"),  // String
+        .MEMORY_SIZE        (XPM_MEM_SIZE),  // Specify the total memory array size, in bits. For example, enter 65536 for a 2kx32 RAM.
+        .MESSAGE_CONTROL    (0),  // DECIMAL
+        .READ_DATA_WIDTH_A  (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .READ_LATENCY_A     (1),  // DECIMAL
+        .READ_RESET_VALUE_A ("0"),  // String
+        .RST_MODE_A         ("ASYNC"),  // String
+        .SIM_ASSERT_CHK     (0),  // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+        .USE_MEM_INIT       (0),  // DECIMAL
+        .USE_MEM_INIT_MMI   (0),  // DECIMAL
+        .WAKEUP_TIME        ("disable_sleep"),  // String
+        .WRITE_DATA_WIDTH_A (TB_AXI4_WORD_NUM_BITS),  // DECIMAL
+        .WRITE_MODE_A       ("read_first"),  // String
+        .WRITE_PROTECT      (1)  // DECIMAL
+    ) xpm_memory_spram_inst_${i} (
+        .clka (clk_cg[${i}]),  // 1-bit input: Clock signal for port A.
+        .rsta (pos_rst),  // 1-bit input: Reset signal for the final port A output register stage.
+        // Synchronously resets output port douta to the value specified by
+        // parameter READ_RESET_VALUE_A.
+        .ena  (axi_mem_req_o[${i}]),  // 1-bit input: Memory enable signal for port A. Must be high on clock
+        // cycles when read or write operations are initiated. Pipelined internally.
+        .addra(axi_mem_addr_10b[${i}]),  // ADDR_WIDTH_A-bit input: Address for port A write and read operations.
+        .dina (axi_mem_wdata_o[${i}]),  // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+        .wea  (axi_mem_be_32b[${i}]),  // WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+        // for port A input data port dina. 1 bit wide when word-wide writes are
+        // used. In byte-wide write configurations, each bit controls the
+        // writing one byte of dina to address addra. For example, to
+        // synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+        // is 32, wea would be 4'b0010.
+        .douta(axi_mem_rdata_i[${i}]),  // READ_DATA_WIDTH_A-bit output: Data output for port A read operations.
+
+        .dbiterra      (),  // 1-bit output: Status signal to indicate double bit error occurrence
+        // on the data output of port A.
+        .sbiterra      (),  // 1-bit output: Status signal to indicate single bit error occurrence
+        // on the data output of port A.
+        .injectdbiterra(1'b0),  // 1-bit input: Controls double bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .injectsbiterra(1'b0),  // 1-bit input: Controls single bit error injection on input data when
+        // ECC enabled (Error injection capability is not available in "decode_only" mode).
+        .regcea        (1'b1),  // 1-bit input: Clock Enable for the last register stage on the output data path.
+        .sleep         (1'b0)  // 1-bit input: sleep signal to enable the dynamic power saving feature.
+    );
 
   always_comb begin
     // Extend the mem_strb_o from 4 bits to 32 bits, each bit must be extended 8 times
@@ -122,7 +175,8 @@ module memory_subsystem
         specific_offset = STORAGE_SPACE_BYTE_SIZE 
         shift = 2 to pass from byte addr to word addr
     */
-    axi_mem_addr_14b[${i}] =  ( axi_mem_addr_o[${i}] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * ${i}  )  >> 2;
+    // axi_mem_addr_14b[${i}] =  ( axi_mem_addr_o[${i}] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * ${i}  )  >> 2;
+    axi_mem_addr_10b[${i}] =  ( axi_mem_addr_o[${i}] - ADDR_MAP_0_START - STORAGE_SPACE_BYTE_SIZE * ${i}  )  >> 2;
     /* axi_mem_we_o = !RDWEN    AND   axi_mem_req_o = !CE  means a read in this clock cycle */
     axi_mem_rvalid_n[${i}] =  /* axi_mem_we_o[${i}] && */ axi_mem_req_o[${i}];
     /* memory always responds after 1 clock cycle for write and read so, if we just delay the reuqest signal to have the gnt */
